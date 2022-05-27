e1cdc6b8aeb11ee1f0a3b3eee3b01fb0f5e101f3
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-05-27 14:31:05 -0300

    fix: refatorando consulta get games com o banco, utilizando o operador join

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -18,12 +18,22 @@ export async function getGames(req, res) {
     try {
         if (name) {
             const consulta = await connection.query(
-                `SELECT * FROM games WHERE name LIKE '${name[0].toUpperCase() + name.substr(1)}%'
-                    OR name LIKE '${name}%' `);
+                `SELECT games.*, categories.name as "categoryName" 
+                    FROM games 
+                    JOIN categories
+                    ON games."categoryId" = categories.id
+                    WHERE games.name LIKE '${name[0].toUpperCase() + name.substr(1)}%'
+                    OR games.name LIKE '${name}%'         
+                `);
             const games = consulta.rows;
             return res.status(200).send(games);
         }
-        const consulta = await connection.query(`SELECT * FROM games`)
+        const consulta = await connection.query(`
+            SELECT games.* , categories.name as "categoryName"
+            FROM games
+            JOIN categories
+            ON games."categoryId" = categories.id
+        `)
         const games = consulta.rows;
         res.status(200).send(games);
     } catch (error) {
